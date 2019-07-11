ab7e131fdb3bb2fa2e015f3a9ab94f24968ab932
catcher <catcher@Catcher-mbp.local>
2019-07-12 00:43:03 +0800

    feature: 配置开发环境

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Sixpence } from './src/pages/index'
+ReactDOM.render(
+  <Sixpence />,
+  document.getElementById('app')
+)
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Sixpence() {
+  return (
+    [<h1>Sixpence</h1>,
+      <p>Hello world!</p>
+    ]
+  )
+}
