c7a34fcd5e75f3ec51453573906944080bbd6155
liuhui <liuhui2583@qq.com>
2019-07-12 19:40:15 +0800

    temp

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router,Switch,Route} from "react-router-dom"
 
-export function Sixpence() {
-  return (
-    [<h1>Sixpence</h1>,
-      <p>Hello world!</p>
-    ]
-  )
+// 页面
+import Detail from './detail/index'
+import login from './login/index'
+import NotFoundPage from './404/index'
+
+class Sixpence extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    {/* <Route exact path="/" component={Detail}/> */}
+                    <Route path="/login" component={Login} />
+                    {/* <Route path="/about/:userId" component={About} /> */}
+                    {/* <Route path="/about" component={About} /> */}
+                    {/* <Route path="/accounts" component={Accounts}/> */}
+                    {/* <Route path="/search/:content" component={Search}/> */}
+                    {/* <Route path="/search" component={Search}/> */}
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </Router>
+        )
+    }
 }
